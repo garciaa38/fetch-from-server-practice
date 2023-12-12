0ee1116150e83f1b658e1b87fc0118e3fa0b62cd
Andrew Garcia <garciaa38@gmail.com>
2023-12-12 18:53:34 -0500

    phase 2 done

--- assets/js/your-code.js
+++ assets/js/your-code.js
@@ -1,19 +1,29 @@
 export function getAllDogs() {
-    // Your code here 
+    const url = '/dogs';
+    const options = {
+        method: 'GET',
+        headers: {'Content-Type': 'text/html'},
+        body: new URLSearchParams({
+            key: 'value'
+        })
+    };
+
+    return fetch(url);
 }
 
 export function getDogNumberTwo() {
-    // Your code here 
+    const url = '/dogs/2';
+    return fetch(url);
 }
 
 export function postNewDog() {
-    // Your code here 
+    // Your code here
 }
 
 export function postNewDogV2(name, age) {
-     // Your code here 
+     // Your code here
 }
 
 export function deleteDog(id) {
-      // Your code here 
+      // Your code here
 }
